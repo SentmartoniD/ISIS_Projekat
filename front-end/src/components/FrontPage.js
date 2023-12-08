4a86b108919cddf918ea5b5bc7f390f2436bfe30
Deneš Sentmartoni <denessentmartoni.com>
2023-12-08 22:45:17 +0100

    Created the initial look of the front page

--- front-end/src/components/FrontPage.js
+++ front-end/src/components/FrontPage.js
@@ -3,8 +3,24 @@
 
 function FrontPage(){
     return(
-        <section>
-            <h1>Electricity consumption forecast</h1>
+        <section className="frontpage-section" >
+            <h1 className="frontpage-h1" >Electricity consumption forecast</h1>
+            <div className="frontpage-div-control" > 
+                <div className="frontpage-div2" >
+                    <label className="frontpage-label" >Load data</label>
+                    <input type="file" ></input>
+                </div>
+                <div className="frontpage-div2">
+                    <label className="frontpage-label" >Weather data</label>
+                    <input type="file" ></input>
+                </div>
+                <div className="frontpage-div2">
+                    <label className="frontpage-label" >US holidays data</label>
+                    <input type="file" ></input>
+                </div>
+                {/* CREATE MODEL BUTTON  */}
+                {/* BEGIN FORECAST*/}
+            </div>
         </section>
     )
 }
