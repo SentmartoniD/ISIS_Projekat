0329cd47d14530ea6f8b787c1bde0431749d9ae1
Deneš Sentmartoni <denessentmartoni.com>
2023-12-15 16:58:59 +0100

    Finalize the SendLoad function wich send multiple posts in a loop which contain smaller chunks

--- front-end/src/services/FrontPageService.js
+++ front-end/src/services/FrontPageService.js
@@ -2,17 +2,27 @@ import axios from "axios"
 
 
 export const SendLoadData = async (files) => {
-    const formData = new FormData();
-    for(let i = 0; i < files.length; i++){
-        formData.append(`file${i + 1}`, files[i]);
+    const chunkSize = 100;
+    const numberOfChunks = Math.ceil(files.length / chunkSize);
+    for (let i = 0; i < numberOfChunks; i++) {
+        const start = i * chunkSize;
+        const end = (i + 1) * chunkSize;
+        const chunk = files.slice(start, end);
+        const formData = new FormData();
+        for (let j = 0; j < chunk.length; j++) {
+            formData.append(`file${j + 1}`, chunk[j]);
+        }
+        try{
+            const response = await axios.post(`${process.env.REACT_APP_PYTHON_APP_API_URL}/api/LoadData`, formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                },
+            });
+            console.log(reposne);
+        }catch(err){
+            console.log(err);
+        }
     }
-    console.log('Payload size:', formData.toString().length / (1024 * 1024), 'MB');
-    return await axios.post(`${process.env.REACT_APP_PYTHON_APP_API_URL}/api/LoadData`, formData, 
-    {
-        headers: {
-            'Content-Type' : 'multipart/form-data'
-        },
-    });
 }
 
 export const SendWeatherData = async (files) => {
