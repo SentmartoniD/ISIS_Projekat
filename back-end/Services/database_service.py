fbce1e80233f9e901c732ba53a080be319b76dc3
Deneš Sentmartoni <denessentmartoni.com>
2023-12-29 23:45:41 +0100

    Added the initial functions for filling up the tables with data

---
+++ back-end/Services/database_service.py
@@ -0,0 +1,13 @@
+from DatabaseFunctions import database_functions
+
+
+def fill_usholidays_table(usholidays_data_files):
+    return
+
+
+def fill_loaddata_table(loaddata_files):
+    return
+
+
+def fill_weatherdata_table(weatherdata_files):
+    return
