647a4e805f320ff14f317ae90f6804353ac2c4c6
Deneš Sentmartoni <denessentmartoni.com>
2024-01-06 16:25:29 +0100

    Fixed the preprocces function

--- back-end/Services/preprocessing_service.py
+++ back-end/Services/preprocessing_service.py
@@ -1,49 +1,39 @@
 from DatabaseFunctions import database_read_functions
 import pandas
 
+SPECIAL_NUMBER = 666999
 
 def preprocess():
     usholidays_list = database_read_functions.read_from_usholidays_table()
     weatherdata_list = database_read_functions.read_from_weatherdata_table()
     loaddata_list = database_read_functions.read_from_loaddata_table()
     data_list = []
-    # GET ALL THE TIMESTAMPS FOR LOAD DATA
     for i in range(loaddata_list.__len__()):
-        elem = []
-        elem.append(loaddata_list[i][1])
-        data_list.append(elem)
-
-    # GET THE VALUES FROM WEATHER DATA
-    for i in range(weatherdata_list.__len__()):
-        for j in range(data_list.__len__()):
-            if (data_list[j][0][6:10] == weatherdata_list[i][2][0:4] and
-                 data_list[j][0][0:2] == weatherdata_list[i][2][5:7] and
-                 data_list[j][0][3:5] == weatherdata_list[i][2][8:10] and
-                 data_list[j][0][11:13] == weatherdata_list[i][2][11:13]):
-                data_list[j].append(weatherdata_list[i][3])
-
-    # GET THE OTHER VALUES FROM LOAD DATA
-    for i in range(loaddata_list.__len__()):
-        for j in range(data_list.__len__()):
-            if loaddata_list[i][1] == data_list[j][0]:
-                data_list[j].append(loaddata_list[i][4])
-                data_list[j].append(loaddata_list[i][5])
+        for j in range(weatherdata_list.__len__()):
+            if (loaddata_list[i][1][6:10] == weatherdata_list[j][2][0:4] and
+                 loaddata_list[i][1][0:2] == weatherdata_list[j][2][5:7] and
+                 loaddata_list[i][1][3:5] == weatherdata_list[j][2][8:10] and
+                 loaddata_list[i][1][11:13] == weatherdata_list[j][2][11:13]):
+                elem = [weatherdata_list[j][3], loaddata_list[i][4], loaddata_list[i][5]]
+                data_list.append(elem)
     '''
-    print(loaddata_list[1][1])
-    print(loaddata_list[2])
-    print(weatherdata_list[2])
-    a = loaddata_list[2]
-    b = weatherdata_list[2]
-    print(weatherdata_list[2][2][0:4])
-    
-    if (a[1][6:10] == b[2][0:4] and a[1][0:2] == b[2][5:7] and a[1][3:5] == b[2][8:10] and a[1][11:13] == b[2][11:13]):
-        print("yes")
-    else:
-        print("no")
-    print(a[1][11:13])
-    print(b[2][11:13])'''
+    s1=0
+    s2=0
+    s3=0
+    for i in range(data_list.__len__()):
+        if data_list[i][0] == SPECIAL_NUMBER:
+            s1 += 1
+        elif data_list[i][1] == SPECIAL_NUMBER:
+            s2 += 1
+        elif data_list[i][2] == SPECIAL_NUMBER:
+            s3 += 1
+    print(f"number of blank spaces in temp is : {s1}")
+    print(f"number of blank spaces in ptid is : {s2}")
+    print(f"number of blank spaces in load is : {s3}")
+    '''
 
-    print(data_list)
+    dataframe = pandas.DataFrame(data_list)
+    print(dataframe)
     return
 
 
