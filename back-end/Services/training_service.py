77c9f3baf57d00a0507a1d89252b83082a662446
Deneš Sentmartoni <denessentmartoni.com>
2024-01-06 17:53:38 +0100

    Added all the helper classes

--- back-end/Services/training_service.py
+++ back-end/Services/training_service.py
@@ -1,6 +1,24 @@
 import preprocessing_service
+from HelperClasses.custom_preparer import CustomPreparer
+from HelperClasses.ann_regression import AnnRegression
+import time
+
+NUMBER_OF_COLUMNS = 3
+SHARE_FOR_TRAINING = 0.85
 
 
 def train_model():
-    dt = preprocessing_service.preprocess()
+    dataframe = preprocessing_service.preprocess()
+
+    # prepare data
+    preparer = CustomPreparer(dataframe, NUMBER_OF_COLUMNS, SHARE_FOR_TRAINING)
+    trainX, trainY, testX, testY = preparer.prepare_for_training()
+
+    # make predictions
+    ann_regression = AnnRegression()
+    time_begin = time.time()
+    trainPredict, testPredict = ann_regression.compile_fit_predict(trainX, trainY, testX)
+    time_end = time.time()
+    print('Training duration: ' + str((time_end - time_begin)) + ' seconds')
+
     return
--- back-end/main.py
+++ back-end/main.py
@@ -1,6 +1,6 @@
 from Controllers.data_controller import app
 from Database import database
-from Services import preprocessing_service
+from Services import training_service
 
 def initialize_database_and_tables():
     database.create_database()
@@ -14,5 +14,5 @@ def initialize_database_and_tables():
 if __name__ == '__main__':
     initialize_database_and_tables()
     #app.run(debug=True)
-    preprocessing_service.preprocess()
+    training_service.train_model()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
